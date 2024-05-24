eabb5965afc42619fd9d7190bf8cb1414eb52a75
Ovais Khan <ovais1070@gmail.com>
2024-05-23 22:22:11 -0400

    Initial Commit

---
+++ Rogram/RogramApp.swift
@@ -0,0 +1,17 @@
+//
+//  RogramApp.swift
+//  Rogram
+//
+//  Created by Ovais Khan on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct RogramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
